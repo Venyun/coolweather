46b5c1c07ea846c904d8e0e8fdec8f73c79841e6
Venny <1457797030@qq.com>
2018-07-22 22:40:37 +0800

    finished

--- app/src/main/java/com/example/venny/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/venny/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.venny.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.venny.coolweather.gson.Forecast;
 import com.example.venny.coolweather.gson.Weather;
+import com.example.venny.coolweather.service.AutoUpdateService;
 import com.example.venny.coolweather.util.HttpUtil;
 import com.example.venny.coolweather.util.Utility;
 
@@ -143,7 +145,8 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.putString("weather",responseText);
                             editor.apply();
                             showWeatherInfo(weather);
-
+                            Intent intent=new Intent(WeatherActivity.this,AutoUpdateService.class);
+                            startService(intent);
                         }else {
                             Toast.makeText(WeatherActivity.this,"require failed",Toast.LENGTH_SHORT).show();
                         }
